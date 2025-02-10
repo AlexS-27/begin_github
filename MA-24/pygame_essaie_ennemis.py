b2912b073b57b4fbab6a178bb8d631ce99340ace
Alex Kamano <pk86onf@eduvaud.ch>
2025-02-10 09:51:09 +0100

    feat: ajout de la fonctionnalité de collision avec réponse en print

--- MA-24/pygame_essaie_ennemis.py
+++ MA-24/pygame_essaie_ennemis.py
@@ -1,9 +1,153 @@
 import pygame
+import math
+
+class Player():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.velocity = 12
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.hitbox_w = self.width + 5
+        self.hitbox_h = self.height + 5
+        self.update_hitbox()
+
+    def update_hitbox(self):
+        self.hitbox_player = pygame.Rect(
+            self.x +(player_w-self.hitbox_w)//2,
+            self.y +(player_h - self.hitbox_h)//2,
+            self.hitbox_w,
+            self.hitbox_h
+        )
+
+    def draw(self, surface):
+
+        pygame.draw.rect(surface, 'red', self.rect)
+        #pygame.draw.rect(surface, 'black', self.hitbox_player,2)
+
+    def move(self, dx, dy):
+        #garde l'ancienne position avant le déplacement
+        old_x = self.x
+        old_y = self.y
+
+        #déplacement
+        self.x += dx
+        self.y += dy
+
+        #Màj du joeur
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.update_hitbox()
+
+        #vérif des limites
+        if self.rect.left < limite_x[0] or self.rect.right > limite_x[1]:
+            self.x = old_x
+            self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        if self.rect.top < limite_y[0] or self.rect.bottom > limite_y[1]:
+            self.y = old_y
+            self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+class Enemy():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.velocity = velocity
+
+        self.hitbox_w = width + 5
+        self.hitbox_h = height + 5
+        self.update_hitbox()
+
+    def update_hitbox(self):
+        #met à jour la hitbox
+        self.hitbox_enemy = pygame.Rect (
+        self.rect.x + (self.width - self.hitbox_w) // 2,
+        self.rect.y + (self.height - self.hitbox_h) // 2,
+        self.hitbox_w,
+        self.hitbox_h
+    )
+
+    def draw(self, window):
+
+        pygame.draw.rect(window, 'green', self.rect)
+        pygame.draw.rect(window, 'black', self.hitbox_enemy, 2)
+
+    def move (self, dx, dy):
+        #on garde l'ancienne position
+        old_ex, old_ey = self.x, self.y
+
+        self.rect.x += dx * velocity
+        self.rect.y += dy * velocity
+
+        #MàJ de l'ennemie
+        #self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        if self.rect.left < limite_x[0] or self.rect.right > limite_x[1]:
+            self.rect.x = old_ex
+            #self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        if self.rect.top < limite_y[0] or self.rect.bottom > limite_y[1]:
+            self.rect.y = old_ey
+            #self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        self.update_hitbox()
+
+    def move_towards_player(self, player, ):
+        #calculer la direction vers le joueur
+        dx = player.rect.x - self.rect.x
+        dy = player.rect.y - self.rect.y
+
+        #calculer la distance entre l'ennemi et le joueur
+        dist = math.hypot(dx, dy)
+
+        # Enemy AI: Move towards the player
+        if dist != 0:
+            dx /= dist
+            dy /= dist
+
+            #déplacer l'ennemi
+            self.rect.x += dx * (self.velocity -2)
+            self.rect.y += dy * (self.velocity -2)
+
+            self.update_hitbox()
+
+class HealthBar():
+
+    def __init__(self, x, y, width, height, max_hp):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.hp = max_hp
+        self.max_hp = max_hp
+
+    def draw(self, surface):
+        #calculate health ratio
+        ratio = self.hp / self.max_hp
+        pygame.draw.rect(window, 'red', (700, 0, 300, 40))
+        pygame.draw.rect(window, 'green', (700, 0, int(300 * ratio), 40))
+
+class Collide_damage():
+    def __init__(self, x, y, max_damage):
+        self.x = x
+        self.y = y
+        self.damage = max_damage
+        self.max_damage = max_damage
+
+    def colide(self, player1, enemy):
+        collide = pygame.Rect.colliderect(enemy.rect, player1.rect)
+
+        #if collide:
+
+
 
 pygame.init()
 
-window = pygame.display.set_mode((1000, 1000))
-
+window_width, window_height = 1000, 1000
+window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Essaie de la Pygame")
 
 limite_x = (60, 940)
@@ -12,37 +156,53 @@ limite_y = (60, 940)
 #store the initial coordinates of the player in two variables i.e x and y
 x, y = 100, 100
 velocity = 12
+player_w, player_h = 50, 50
 
-player = pygame.Rect((x ,y ,50,50))
+ex, ey = 720, 720
+enemy_w, enemy_h = 50, 50
+enemy_speed = 2
 
+player1 = Player(x, y, player_w, player_h)
+enemy = Enemy(x, y, enemy_w, enemy_h)
+
+health_bar = HealthBar(250,200,300,40,100)
 run = True
+clock = pygame.time.Clock()
+
 while run:
     pygame.time.delay(30)
     window.fill((255,255,255))
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        player1.move(-player1.velocity, 0)
+    if key[pygame.K_RIGHT]:
+        player1.move(player1.velocity, 0)
+    if key[pygame.K_UP]:
+        player1.move(0, -player1.velocity)
+    if key[pygame.K_DOWN]:
+        player1.move(0, player1.velocity)
+
+    if player1.hitbox_player.colliderect(enemy.hitbox_enemy):
+        print("collision")
+
+    #draw health bar
+    health_bar.hp = 50
+    health_bar.draw(window)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             pygame.quit()
 
-    old_x, old_y = player.x, player.y
+    # Enemy AI: Move towards the player
+    enemy.move_towards_player(player1)
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_LEFT]:
-        player.x -= velocity
-    if key[pygame.K_RIGHT]:
-        player.x += velocity
-    if key[pygame.K_UP]:
-        player.y -= velocity
-    if key[pygame.K_DOWN]:
-        player.y += velocity
+    #pygame.draw.rect(window, (255,0,0), player)
+    player1.draw(window)
+    enemy.draw(window)
 
-    if player.left < limite_x[0] or player.right > limite_x[1]:
-        player.x = old_x
-    if player.top < limite_y[0] or player.bottom > limite_y[1]:
-        player.y = old_y
-
-    pygame.draw.rect(window, (255,0,0), player)
+    pygame.display.flip()
 
     pygame.display.update()
 
